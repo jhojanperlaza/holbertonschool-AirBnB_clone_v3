76a28f08e6b53dbb099c2cb65e4007ec001c5c45
jhojan <jhojan_perlaza123>
2022-09-16 11:26:24 -0500

    [UPDATED] index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,29 @@ initialize
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
 
 @app_views.route('/status', strict_slashes=False)
 def index():
     """returns returns a JSON: status: OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def cout():
+    """returns returns a JSON: status: OK"""
+    dict_return = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+
+    return jsonify(dict_return)
