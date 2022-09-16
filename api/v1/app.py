f3a4676d64da8bf0e6fff97bb36e05f76ae21c01
AFOP <4592@holbertonschool.com>
2022-09-16 11:56:31 -0500

    Task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from models import storage
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -16,6 +17,10 @@ def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found():
+    # note that we set the 404 status explicitly
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
     if (getenv("HBNB_API_HOST") and getenv("HBNB_API_PORT")):
