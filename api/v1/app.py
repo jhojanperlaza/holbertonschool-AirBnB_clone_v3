05b91b76a240b5d2195b64b686267609e797611d
jhojan <jhojan_perlaza123>
2022-09-16 01:18:11 -0500

    [UPDATED] files api

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,20 +5,22 @@ starts a Flask web application
 
 from models import storage
 from os import getenv
-import api.v1.views from app_views
+from api.v1.views import app_views
 from flask import Flask
 app = Flask(__name__)
+app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
+
 if __name__ == '__main__':
     if (getenv("HBNB_API_HOST") and getenv("HBNB_API_PORT")):
         api_host = getenv("HBNB_API_HOST")
         api_port = getenv("HBNB_API_PORT")
-        app.run(host=api_host, port=api_port)
+        app.run(host=api_host, port=api_port, threaded=True)
     else:
-        app.run(host='0.0.0.0', port='5000')
-
+        app.run(host='0.0.0.0', port='5000', threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,12 +4,9 @@ initialize
 """
 
 from api.v1.views import app_views
-from flask import Flask
 from flask import jsonify
-app = app_views
 
-@app.route('/status', strict_slashes=False)
-
+@app_views.route('/status', strict_slashes=False)
 def index():
     """returns returns a JSON: status: OK"""
-    return jsonify("status: OK")
+    return jsonify({"status": "OK"})
