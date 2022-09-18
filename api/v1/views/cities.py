bab9ec4fa5a6afc764cc2a62a343a52e7c928199
jhojan <jhojan_perlaza123>
2022-09-17 21:51:39 -0500

    [UPDATED] file cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -45,14 +45,11 @@ def delete_citys(city_id):
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
 def post_citys(state_id):
     """transform the HTTP body request to a dictionary"""
+    if not request.json:
+        return ("Not a JSON"), 400
+    if 'name' not in request.json:
+        return ("Missing name"), 400
     linked_states = storage.get(State, state_id)
-    if not linked_states:
-        abort(404)
-    new_city = request.get_json()
-    if not new_city:
-        abort(400, "Not a JSON")
-    if "name" not in new_city:
-        abort(400, "Missing name")
     if linked_states:
         data = request.json
         new_inst = City()
@@ -61,9 +58,10 @@ def post_citys(state_id):
             setattr(new_inst, 'state_id', state_id)
         storage.new(new_inst)
         storage.save()
-        return make_response(jsonify(new_inst.to_dict()), 201)
+        return  make_response(jsonify(new_inst.to_dict()), 201)
     else:
         abort(400)
+
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'],
