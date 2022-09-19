2baa9ff49d8dd1158ea12cced5fb42a949623088
jhojan <jhojan_perlaza123>
2022-09-19 13:19:39 -0500

    [ADD] file user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -10,19 +10,19 @@ from models.user import User
 from models import storage
 
 
-@app_views.route('/users')
+@app_views.route('/users', strict_slashes=False)
 def get_User():
-    """Retrieves the list of all User objects"""
+    """Retrieves the list of all State objects"""
     all_obj = storage.all(User)
-    if all_obj:
-        return jsonify(all_obj)
-    else:
-        abort(404)
+    lista = []
+    for obj in all_obj.values():
+        lista.append(obj.to_dict())
+    return jsonify(lista)
 
 
-@app_views.route('/users/<user_id>', strict_slashes=False, methods=['GET'])
+@app_views.route('/users/<user_id>', methods=['GET'])
 def get_User_id(user_id):
-    """Retrieves the list of all User objects with id"""
+    """Retrieves the list of all Amenity objects with id"""
     linked_user = storage.get(User, user_id)
     if linked_user:
         return jsonify(linked_user.to_dict())
@@ -41,30 +41,42 @@ def delete_User(user_id):
     else:
         abort(404)
 
-
 @app_views.route('/users', methods=['POST'])
 def post_User():
-    """the new user in data base"""
-    if not request.get_json(request):
-        return ("Not a JSON"), 400
-    if 'name' not in request.get_json(request):
-        return ("Missing name"), 400
-    all_users = storage.all(User)
-    if all_users:
-        data = request.get_json(request)
-        new_inst = User()
-        for k, v in data.items():
-            setattr(new_inst, k, v)
+    """ Creates a City object """
+    new_user = request.get_json(request)
+    if not new_user:
+        abort(400, "Not a JSON")
+    if "email" not in new_user:
+        abort(400, "Missing email")
+    if "password" not in new_user:
+        abort(400, "Missing password")
+    data = request.json
+    new_inst = User()
+    for k, v in data.items():
+        setattr(new_inst, k, v)
         storage.new(new_inst)
         storage.save()
-        return new_inst.to_dict(), 201
-    else:
-        abort(400)
 
 
 @app_views.route('/users/<user_id>', strict_slashes=False, methods=['PUT'])
 def put_User(user_id):
     """Update a name of User"""
+    linked_user = storage.get(User, user_id)
+    if linked_user:
+        data = request.get_json(request)
+        if not data:
+            return ("Not a JSON"), 400
+        for k, v in data.items():
+            setattr(linked_user, k, v)
+        storage.save()
+        return linked_user.to_dict(), 200
+    else:
+        abort(404)
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def put_User(user_id):
+    """Update a name of amenity"""
     linked_user = storage.get(User, user_id)
     if linked_user:
         data = request.get_json(request)
